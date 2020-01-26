37f9249682e506ba9fcecee832a47844df5fab51
Jeremy Thorne <jez@jezandbecks.co.uk>
2020-01-26 22:18:43 +0000

    more porting, MyActor and Eagle classes

--- bunner-master/Sources/bunner/main.swift
+++ bunner-master/Sources/bunner/main.swift
@@ -7,12 +7,39 @@ let TITLE = "Infinite Bunner"
 let ROW_HEIGHT = 40
 
 class MyActor: sgz.Actor {
-    init(image:String, pos:(x:Float, y:Float)) {
-        //TODO support anchor points
-        super.init(image:image, center:pos)
-    } 
+    var children = [MyActor]()
+    override init(image:String, pos:(x:Float, y:Float),
+         anchor:(x:sgz.Anchor, y:sgz.Anchor) = 
+            (sgz.Anchor.center, sgz.Anchor.bottom)) {
+        super.init(image:image, pos:pos, anchor:anchor)
+    }
+
+    func draw(app:sgz.App, offset_x:Float, offset_y:Float) {
+        self.x += offset_x
+        self.y += offset_y
+        self.draw(app:app)
+        for child in self.children {
+            child.draw(app:app, offset_x:self.x, offset_y:self.y)
+        }
+        self.x -= offset_x
+        self.y -= offset_y
+    }
+
+    func update(app:sgz.App, game:MyGame) {
+        for child in self.children {
+            child.update(app:app, game:game)
+        }
+    }
 }
 
+class Eagle : MyActor {
+    init(pos:(x:Float, y:Float)) {
+        super.init(image:"eagle", pos:pos)
+    }
+}
+
+class MyGame {
+}
 
 class UI:sgz.Game {
     override func update(app:sgz.App) {
