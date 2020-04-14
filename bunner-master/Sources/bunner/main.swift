f37df410d19845b933bc15bcf84d248992804b8f
Jeremy Thorne <jeremythorne@github.com>
2020-04-14 11:44:21 +0100

    now rendering! with basic game loop
    bunner, grass and eagle all render. Scene is filled with grass rows which scroll
    down, when bunner leaves bottom eagle flies down and game over displayed.

--- bunner-master/Sources/bunner/main.swift
+++ bunner-master/Sources/bunner/main.swift
@@ -75,17 +75,17 @@ class Bunner : MyActor {
     var direction = 2
     var timer = 2
     var input_queue:[Int] = []
-    var min_y:Float
+    var min_y:Float = 0
 
     init(pos:(x:Float, y:Float)) {
         super.init(image:"blank", pos:pos)
-        min_y = self.y
+        min_y = y
     }
 
     func handle_input(app:sgz.App, game:MyGame, dir:Int) {
-        for row_o in game.rows {
-            if let row = row_o, row.y == y + MOVE_DISTANCE * DY[dir] {
-                if row.allow_movement(x + MOVE_DISTANCE * DX[dir]) {
+        for row in game.rows {
+            if row.y == y + MOVE_DISTANCE * DY[dir] {
+                if row.allow_movement(app:app, x + MOVE_DISTANCE * DX[dir]) {
                     direction = dir
                     timer = Int(MOVE_DISTANCE)
                     game.play_sound(app:app, "jump", 1)
@@ -117,14 +117,14 @@ class Bunner : MyActor {
             }
             var current_row_o:Row? = nil
             for row in game.rows {
-                if row?y == y {
+                if row.y == y {
                     current_row_o = row
                     break
                 }
             }
 
             if let current_row = current_row_o {
-                var col = current_row.check_collision(x)
+                let col = current_row.check_collision(x)
                 state = col.state
                 if state == PlayerState.ALIVE {
                     x += current_row.push()
@@ -168,24 +168,26 @@ class Bunner : MyActor {
     }
 }
 
-class Row : MyActor {
+typealias Row = RowBase & NextRow
+
+protocol NextRow {
+    func next() -> Row
+}
+
+class RowBase : MyActor {
     var index:Int
     var dx:Float = 0
     init(base_image:String, index:Int, y:Float) {
         self.index = index
         super.init(image:base_image + String(index),
                    pos:(0, y),
-                   anchor:(sgz.Anchor.Left, sgz.Anchor.Bottom))
+                   anchor:(sgz.Anchor.left, sgz.Anchor.bottom))
     }
 
-    func next() -> Row? {
-        return nil
-    }
-
-    func collide(x:Float, margin:Float = 0) -> MyActor? {
+    func collide(app:sgz.App, x:Float, margin:Float = 0) -> MyActor? {
         for child in children {
-            if x >= child.x - (child.width / 2) - margin &&
-                x < child.x + (child.width / 2) + margin {
+            if x >= child.x - (Float(child.width(app:app)) / 2) - margin &&
+                x < child.x + (Float(child.width(app:app)) / 2) + margin {
                 return child
             }
         }
@@ -199,7 +201,7 @@ class Row : MyActor {
     func play_sound(app:sgz.App, game:MyGame) {
     }
 
-    func allow_movement(_ x:Float) -> Bool {
+    func allow_movement(app:sgz.App, _ x:Float) -> Bool {
         return x >= 16 && x <= WIDTH - 16
     }
 
@@ -209,47 +211,56 @@ class Row : MyActor {
     }
 }
 
+func generate_hedge_mask() -> [Bool] {
+    return Array(repeating:false, count:17)
+}
+
+func classify_hedge_segment(_ mask:[Bool], _ mid_segment:Bool?) ->
+    (mid_segment:Bool, sprite_x:Float?) {
+    return (false, nil)
+}
+
 class Grass : Row {
     var hedge_row_index:Int?
-    var hedge_mask:[Bool]?
+    var hedge_mask = Array(repeating:false, count:17)
     init(predecessor:Row?, index:Int, y:Float) {
+        super.init(base_image:"grass", index:index, y:y)
         print("Grass::init \(index) \(y)")
-        if !predecessor is Grass ||
-            let grass = predecessor as! Grass,
-             grass.hedge_row_index = nil {
-            if Float.random(in:0...<1) < 0.5 && index > 7 && index < 14 {
-                hedge_mask = game.generate_hedge_mask()
+        let grass = predecessor as? Grass
+        if grass == nil || grass!.hedge_row_index == nil {
+            if Float.random(in:0..<1) < 0.5 && index > 7 && index < 14 {
+                hedge_mask = generate_hedge_mask()
                 hedge_row_index = 0
             }                
-        } else if let grass = predecessor as! Grass,
-             grass.hedge_row_index = 0 {
-            hedge_mask = grass.hedge_mask
+        } else if grass!.hedge_row_index == 0 {
+            hedge_mask = grass!.hedge_mask
             hedge_row_index = 1
         }
         if hedge_row_index != nil {
             var previous_mid_segment:Bool?
             for i in 1...13 {
-                var seg = game.classify_hedge_segment(
-                    hedge_mask[(i - 1)...(i + 3)], previous_mid_segment)
+                let seg = classify_hedge_segment(
+                    Array(hedge_mask[(i - 1)...(i + 3)]), previous_mid_segment)
                 previous_mid_segment = seg.mid_segment
                 if seg.sprite_x != nil {
-                    children.append(
-                        Hedge(x:seg.sprite_x, index:hedge_row_index,
-                              (i * 40 - 20, 0)))
+                    // TODO Hedge
+                    //children.append(
+                    //    Hedge(x:seg.sprite_x, index:hedge_row_index,
+                    //          (i * 40 - 20, 0)))
                 }
             }
         }
-        }
+    }
 
-    override func allow_movement(_ x:Float) -> Bool {
-        return super.allow_movement(x) && collide(x: x, margin: 8) != nil
+    override func allow_movement(app:sgz.App, _ x:Float) -> Bool {
+        return super.allow_movement(app:app, x) && collide(app:app, x: x, margin: 8) != nil
     }
     
     override func play_sound(app:sgz.App, game:MyGame) {
         game.play_sound(app:app, "grass", 1)
     }
 
-    override func next() -> Row? {
+    func next() -> Row {
         print("Grass::next")
         let new_y = y - Float(ROW_HEIGHT)
         switch(index) {
@@ -262,17 +273,18 @@ class Grass : Row {
         case 9...14:
             return Grass(predecessor:self, index:index + 1, y:new_y)
         default:
-            if Float.random(in:0..<1) < 0.5 {
-                return nil //TODO return Road
-            }
-            return nil //TODO return Water
+            return Grass(predecessor:self, index:0, y:new_y)
+        //    if Float.random(in:0..<1) < 0.5 {
+        //        return nil //TODO return Road
+        //    }
+        //    return nil //TODO return Water
         }
     }
 }
 
 class MyGame {
     var bunner:Bunner?
-    var rows:[Row?] = [Grass(predecessor:nil, index:0, y:0)]
+    var rows:[Row] = [Grass(predecessor:nil, index:0, y:0)]
     var scroll_pos:Float = -HEIGHT
     var eagle:Eagle?
     var frame = 0
@@ -290,24 +302,22 @@ class MyGame {
             scroll_pos -= 1
         }
         
-        var new_rows = [Row?]()
-        for row_o in rows {
-            if let row = row_o {
-                if row.y < scroll_pos + HEIGHT + Float(ROW_HEIGHT) * 2 {
-                    new_rows.append(row)
-                }
+        var new_rows = [Row]()
+        for row in rows {
+            if row.y < scroll_pos + HEIGHT + Float(ROW_HEIGHT) * 2 {
+                new_rows.append(row)
             }
         }
         rows = new_rows
 
-        while let row = rows[rows.count - 1], row.y > scroll_pos + Float(ROW_HEIGHT) {
+        var row = rows[rows.count - 1]
+        while row.y > scroll_pos + Float(ROW_HEIGHT) {
             rows.append(row.next())
+            row = rows[rows.count - 1]
         }
 
-        for row_o in rows {
-            if let row = row_o {
-                row.update(app:app, game:self)
-            }
+        for row in rows {
+            row.update(app:app, game:self)
         }
         if let bunner = self.bunner {
             bunner.update(app:app, game:self)
@@ -315,16 +325,15 @@ class MyGame {
         if let eagle = self.eagle {
             eagle.update(app:app, game:self)
         }
-
+        print("MyGame::update end")
         // TODO sounds
     }
 
     func draw(app:sgz.App) {
+        print("MyGame::draw")
         var all_objs = [MyActor]()
-        for row_o in rows {
-            if let row = row_o {
-                all_objs.append(row)
-            }
+        for row in rows {
+            all_objs.append(row)
         }
 
         if let bunner = self.bunner {
@@ -341,6 +350,7 @@ class MyGame {
         }
 
         _ = all_objs.map { $0.draw(app:app, 0, -scroll_pos) }
+        print("MyGame::draw end")
     }
 
     func score() -> Int {
@@ -351,7 +361,7 @@ class MyGame {
     }
 
     func play_sound(app:sgz.App, _ sound:String, _ count:Int = 0) {
-        if self.bunner != nil {
+        if self.bunner != nil && count > 0 {
             app.playSound(name:sound + String(Int.random(in:0..<count)))
         }
     }
@@ -360,14 +370,6 @@ class MyGame {
         return false
     }
 
-    func generate_hedge_mask() -> [Bool] {
-        return [Bool]()
-    }
-
-    func classify_hedge_segment(_ mask:[Bool], _ mid_segment:[Bool]?) ->
-        (mid_segment:Bool, sprite_x:Float?) {
-        return (false, nil)
-    }
 }
 
 enum State:Int {
@@ -383,23 +385,24 @@ class UI:sgz.Game {
 
     override func update(app:sgz.App) {
         switch state {
-        case State.MENU:
+        case .MENU:
             if app.pressed(KeyCode.space) {
                 state = State.PLAY
                 game = MyGame(bunner:Bunner(pos:(240, -320)))
             } else {
                 game.update(app:app)
             }
-        case State.PLAY:
-            if game?bunner.state != PlayerState.ALIVE &&
-                game?bunner.timer < 0 {
+        case .PLAY:
+            if let bunner = game.bunner,
+                    bunner.state != PlayerState.ALIVE &&
+                    bunner.timer < 0 {
                 high_score = max(high_score, game.score())
                 //TODO save high_score to file
                 state = State.GAME_OVER
             } else {
                 game.update(app:app)
             }
-        case State.GAME_OVER:
+        case .GAME_OVER:
             if app.pressed(KeyCode.space) {
                 state = State.MENU
                 game = MyGame()
@@ -412,27 +415,31 @@ class UI:sgz.Game {
         var i = 0
         let ns = String(n)
         for c in ns {
-            app.blit(name:"digit\(colour)" + c,
-                     pos:(x + (i - ns.count * align) * 25, 0))
+            //app.blit(name:"digit\(colour)" + c,
+            //         pos:(Float(x + (i - ns.count * align) * 25), 0))
             i += 1
         }
     }
 
     override func draw(app:sgz.App) {
+        print("UI::draw")
         game.draw(app:app)
         switch state {
-        case State.MENU:
+        case .MENU:
+            print("case Menu")
             app.blit(name:"title", pos:(0, 0))
             let start_index = 
-                [0, 1, 2, 1][(Int(game.scroll_pos) / 6) % 4]
+                [0, 1, 2, 1][max(Int(game.scroll_pos) / 6, 0) % 4]
+            print("start_index \(start_index)")
             app.blit(name:"start" + String(start_index),
                 pos:((WIDTH - 270) / 2, HEIGHT - 240))
-        case State.PLAY:
+        case .PLAY:
             display_number(app:app, game.score(), 0, 0, 0)
             display_number(app:app, high_score, 1, Int(WIDTH) - 10, 1)
-        case State.GAME_OVER:
+        case .GAME_OVER:
             app.blit(name:"gameover", pos:(0,0))
         }
+        print("UI::draw end")
     }
 
 }
